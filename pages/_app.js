30c94220a02da9e1bf354a1147d99171337945bf
Bruno <bruno.crosier@gmail.com>
2021-02-21 19:39:45 +0000

    log pageProps to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { Provider } from 'next-auth/client'
 import './styles.css'
+import { useEffect } from 'react'
 
 // Use the <Provider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App ({ Component, pageProps }) {
+  useEffect(() => {
+    console.log(pageProps);
+  }, [pageProps]);
   return (
     <Provider
       // Provider options are not required but can be useful in situations where
